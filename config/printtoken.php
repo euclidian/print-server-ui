79dac4f98b92a223bb874a1013504f58f1af402b
Ahmad Hasya <hasyazidane2@gmail.com>
2019-07-26 10:17:22 +0700

    URL LOGIN tidak ada default

--- config/printtoken.php
+++ config/printtoken.php
@@ -4,5 +4,5 @@ return [
     "grant_type" => env('PRINT_API_GRANT_TYPE', "password"),
     "client_id" => env('PRINT_API_CLIENT_ID'),
     "client_secret" => env('PRINT_API_CLIENT_SECRET'),
-    "url_login" => env('PRINT_API_URL_LOGIN', "https://print.asmatende.asmat.app/oauth/token")
+    "url_login" => env('PRINT_API_URL_LOGIN')
 ];
