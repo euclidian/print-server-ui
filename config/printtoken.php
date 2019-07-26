e6d3c10930cc71e08e529a614ddebd3e5cfd2730
ahmadhasya <52899635+ahmadhasya@users.noreply.github.com>
2019-07-26 10:18:20 +0700

    Login Api (#11)
    * Login Api
    
    * URL LOGIN tidak ada default

---
+++ config/printtoken.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "grant_type" => env('PRINT_API_GRANT_TYPE', "password"),
+    "client_id" => env('PRINT_API_CLIENT_ID'),
+    "client_secret" => env('PRINT_API_CLIENT_SECRET'),
+    "url_login" => env('PRINT_API_URL_LOGIN')
+];
