ae991fe7f400883afe2a40109a9df64df3cb3460
Rey Muhamad Rifqi <reymuh72@gmail.com>
2019-07-29 09:29:36 +0700

    Delete Token Value

--- config/printtoken.php
+++ config/printtoken.php
@@ -2,7 +2,7 @@
 
 return [
     "grant_type" => env('PRINT_API_GRANT_TYPE', "password"),
-    "client_id" => env('PRINT_API_CLIENT_ID', '6'),
-    "client_secret" => env('PRINT_API_CLIENT_SECRET', 'RFzswjrtSyqGnyb2gDGAVoLzLcfjJkVpVofAMBBT'),
-    "url_login" => env('PRINT_API_URL_LOGIN', 'https://print.asmatende.asmat.app/oauth/token')
+    "client_id" => env('PRINT_API_CLIENT_ID'),
+    "client_secret" => env('PRINT_API_CLIENT_SECRET'),
+    "url_login" => env('PRINT_API_URL_LOGIN')
 ];
